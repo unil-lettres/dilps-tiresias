b64194153f4d14584b2274dca04b148c585d2486
Luca Guindani <luca.guindani@gmail.com>
2020-09-11 10:12:30 +0200

    Update dependencies to Angular 10

--- client/app/shared/components/AbstractDetail.ts
+++ client/app/shared/components/AbstractDetail.ts
@@ -1,10 +1,11 @@
-import {OnInit} from '@angular/core';
+import {OnInit, Directive} from '@angular/core';
 import {MatDialogRef} from '@angular/material/dialog';
 import {merge} from 'lodash';
 import {ArtistComponent} from '../../artists/artist/artist.component';
 import {UserService} from '../../users/services/user.service';
 import {AlertService} from './alert/alert.service';
 
+@Directive()
 export class AbstractDetail implements OnInit {
     public user;
 
