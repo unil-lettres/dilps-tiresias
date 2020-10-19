8f4707ffcb912bd5064ebc9ca555b7595cdec145
Luca Guindani <luca.guindani@gmail.com>
2020-10-19 16:22:54 +0200

    Updated travis configuration file Removed client/environments/environment.*.ts files

--- client/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-// The file contents for the current environment will overwrite these during build.
-// The scripts/setenv.js script is used to generate an environment file with the variables found in the .env file.
-// The build system defaults to the dev environment which uses `environment.ts`, but if you do
-// `ng build --env=prod` then `environment.prod.ts` will be used instead.
