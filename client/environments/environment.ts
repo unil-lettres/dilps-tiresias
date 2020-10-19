190d6c7fe0f85c0baf5cc313719260ce9efdea46
Luca Guindani <luca.guindani@gmail.com>
2020-10-19 16:01:11 +0200

    Cannot remove client/environments/environment.*.ts files

---
+++ client/environments/environment.ts
@@ -0,0 +1,3 @@
+// The file contents for the current environment will overwrite these during build.
+// The scripts/setenv.js script is used to generate an environment file with the variables found in the .env file.
+// The build system defaults to the dev environment which uses `environment.ts`.
