79c5a02ee930edfe4a4c95b1fb3297710e8f1a19
Luca Guindani <luca.guindani@gmail.com>
2020-10-20 14:56:19 +0200

    Add environment variables for angular (#73)
    * Merge develop
    
    * Cannot remove client/environments/environment.*.ts files
    
    * Updated travis configuration file
    Removed client/environments/environment.*.ts files
    
    * Update travis configuration file
    
    * Updated package.json scripts
    Updated setenv.ts script
    
    * Refactor setenv.ts
    Add prettier to codegen script
    
    * Fix package.json scripts
    
    * Refactor package.json scripts

--- client/environments/environment.stage.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    environment: 'staging',
-    agmApiKey: 'AIzaSyDlCHI186ulZTpyTUO9l8PPsD38W1a6i0k',
-    bugsnagApiKey: '1995a31a02f4d32d40e36840f8c81509',
-};
---
+++ scripts/setenv.ts
@@ -0,0 +1,49 @@
+const {writeFile} = require('fs');
+const {argv} = require('yargs');
+
+// Read environment variables from .env file
+require('dotenv').config();
+
+function createEnvironmentFile(path: string, env: string): void {
+    const data = {
+        environment: env,
+        agmApiKey: process.env.MAPS_API_KEY,
+        bugsnagApiKey: process.env.BUGSNAG_API_KEY,
+    };
+    const environmentFileContent = `export const environment = ${JSON.stringify(data)};`;
+
+    // Write the content to the respective file
+    writeFile(path, environmentFileContent, err => {
+        if (err) {
+            console.log(err);
+        }
+        console.log(`Created environment file in ${path}`);
+    });
+}
+
+// Read the command line arguments passed with yargs
+const configuration = argv.configuration;
+let targetPath = './client/environments/environment.ts';
+let environment = 'development';
+
+// Create base environment file
+createEnvironmentFile(targetPath, environment);
+
+switch (configuration) {
+    case 'production':
+        console.log('Creating production environment file.');
+        targetPath = './client/environments/environment.prod.ts';
+        environment = 'production';
+
+        // Create specific environment file
+        createEnvironmentFile(targetPath, environment);
+        break;
+    case 'staging':
+        console.log('Creating staging environment file.');
+        targetPath = './client/environments/environment.stage.ts';
+        environment = 'staging';
+
+        // Create specific environment file
+        createEnvironmentFile(targetPath, environment);
+        break;
+}
