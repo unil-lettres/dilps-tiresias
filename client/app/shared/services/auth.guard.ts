067cd42165f5a138a2d84d18e69365c565e4cd66
Luca Guindani <luca.guindani@gmail.com>
2020-11-17 09:47:50 +0100

    Tiresias visitors should be automatically logged as guest (#75)

--- client/app/shared/services/auth.guard.ts
+++ client/app/shared/services/auth.guard.ts
@@ -1,14 +1,18 @@
-import {Injectable} from '@angular/core';
+import {Inject, Injectable} from '@angular/core';
 import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot} from '@angular/router';
 import {map} from 'rxjs/operators';
 import {UserService} from '../../users/services/user.service';
+import {SITE} from '../../app.config';
 import {Observable} from 'rxjs';
+import {Site} from '../generated-types';
 
 @Injectable({
     providedIn: 'root',
 })
 export class AuthGuard implements CanActivate {
-    constructor(private router: Router, private userService: UserService) {}
+    public Site = Site;
+
+    constructor(private router: Router, private userService: UserService, @Inject(SITE) public site: Site) {}
 
     /**
      * App need user to be connected or explicit action to access the inner content. Login service provide anonymous user in second case
@@ -18,10 +22,21 @@ export class AuthGuard implements CanActivate {
         return this.userService.getCurrentUser().pipe(
             map(user => {
                 if (!user && !this.userService.hasTempAccess()) {
-                    this.router.navigate(['/login'], {queryParams: {returnUrl: state.url}});
-                    return false;
+                    switch (this.site) {
+                        // Tiresias visitors should be automatically logged as guest
+                        case Site.tiresias:
+                            this.userService.startTempAccess();
+                            this.router.navigate([state.url]);
+                            return true;
+                        // Dilps visitors should be redirected to the login view
+                        case Site.dilps:
+                        default:
+                            this.router.navigate(['/login'], {queryParams: {returnUrl: state.url}});
+                            return false;
+                    }
                 }
 
+                // Users should always validate the terms agreement
                 if (user && !user.termsAgreement) {
                     this.router.navigate(['/login'], {queryParams: {returnUrl: state.url}});
                     return true;
