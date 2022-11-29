f7718b4b07c6b8a32f36830b128d4caa1af98602
Adrien Crivelli <adrien.crivelli@gmail.com>
2022-11-29 11:54:34 +0100

    Skip Bugsnag if config is empty
    This avoids annoying warning during development

--- client/app/shared/config/bugsnag.ts
+++ client/app/shared/config/bugsnag.ts
@@ -1,15 +1,22 @@
 import Bugsnag from '@bugsnag/js';
 import {environment} from '../../../environments/environment';
 import {BugsnagErrorHandler} from '@bugsnag/plugin-angular';
-
-/** Bugsnag client configuration */
-const bugsnagClient = Bugsnag.start({
-    apiKey: environment.bugsnagApiKey,
-    releaseStage: environment.environment,
-    enabledReleaseStages: ['production', 'staging'],
-});
+import {ErrorHandler} from '@angular/core';
 
 /** Factory which will return the Bugsnag error handler */
-export function bugsnagErrorHandlerFactory(): BugsnagErrorHandler {
-    return new BugsnagErrorHandler(bugsnagClient);
+export function bugsnagErrorHandlerFactory(): ErrorHandler {
+    const apiKey = environment.bugsnagApiKey;
+    console.log(apiKey);
+    if (apiKey) {
+        // Bugsnag client configuration
+        const bugsnagClient = Bugsnag.start({
+            apiKey: apiKey,
+            releaseStage: environment.environment,
+            enabledReleaseStages: ['production', 'staging'],
+        });
+
+        return new BugsnagErrorHandler(bugsnagClient);
+    }
+
+    return new ErrorHandler();
 }
