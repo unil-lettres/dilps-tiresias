2b529934b6dfbc7a3804cecc234261b6a07fa232
Adrien Crivelli <adrien.crivelli@gmail.com>
2022-11-29 15:19:09 +0100

    Only update artist cache of impacted cards #9224
    Because DB is huge, it's important to only treat records that may have
    been touched, and not the whole DB.
    
    The most complex case is artist deletion where we need to make a list of
    touched cards before we actually delete the artist. But the cache update
    can only be done after the artist is deleted. To do that we use a
    temporary table, `updated_card`, to pass data across triggers.

--- data/triggers.sql
+++ data/triggers.sql
@@ -1,14 +1,17 @@
 DELIMITER ~~
 
-CREATE OR REPLACE PROCEDURE update_card_artists ()
+CREATE OR REPLACE PROCEDURE update_card_artists (IN card_id INT)
 BEGIN
 
     UPDATE card LEFT JOIN (
-        SELECT card_artist.card_id, IFNULL(GROUP_CONCAT(artist.name ORDER BY artist.id SEPARATOR '\n'), '') AS artists FROM card_artist
+        SELECT card_artist.card_id, IFNULL(GROUP_CONCAT(artist.name ORDER BY artist.id SEPARATOR '\n'), '') AS artists
+        FROM card_artist
         INNER JOIN artist ON card_artist.artist_id = artist.id
+        WHERE card_artist.card_id = card_id
         GROUP BY card_artist.card_id
     ) AS tmp ON card.id = tmp.card_id
     SET cached_artist_names = IFNULL(tmp.artists, '')
+    WHERE card.id = card_id
     ;
 END ~~
 
@@ -17,7 +20,7 @@ CREATE OR REPLACE TRIGGER card_artist_after_insert
     ON card_artist
     FOR EACH ROW
 BEGIN
-    CALL update_card_artists();
+    CALL update_card_artists(NEW.card_id);
 END; ~~
 
 CREATE OR REPLACE TRIGGER card_artist_after_update
@@ -25,7 +28,8 @@ CREATE OR REPLACE TRIGGER card_artist_after_update
     ON card_artist
     FOR EACH ROW
 BEGIN
-    CALL update_card_artists();
+    CALL update_card_artists(OLD.card_id);
+    CALL update_card_artists(NEW.card_id);
 END; ~~
 
 CREATE OR REPLACE TRIGGER card_artist_after_delete
@@ -33,7 +37,7 @@ CREATE OR REPLACE TRIGGER card_artist_after_delete
     ON card_artist
     FOR EACH ROW
 BEGIN
-    CALL update_card_artists();
+    CALL update_card_artists(OLD.card_id);
 END; ~~
 
 
@@ -42,7 +46,39 @@ CREATE OR REPLACE TRIGGER artist_after_update
     ON artist
     FOR EACH ROW
 BEGIN
-    CALL update_card_artists();
+
+    DECLARE done INT DEFAULT FALSE;
+    DECLARE card_id INT;
+    DECLARE cur CURSOR FOR SELECT card_artist.card_id
+                           FROM card_artist
+                           WHERE card_artist.artist_id = NEW.id;
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
+
+    OPEN cur;
+
+    read_loop:
+    LOOP
+        FETCH cur INTO card_id;
+
+        IF done
+        THEN
+            LEAVE read_loop;
+        END IF;
+
+        CALL update_card_artists(card_id);
+    END LOOP;
+
+    CLOSE cur;
+
+END; ~~
+
+CREATE OR REPLACE TRIGGER artist_before_delete
+    BEFORE DELETE
+    ON artist
+    FOR EACH ROW
+BEGIN
+    CREATE TEMPORARY TABLE IF NOT EXISTS updated_card (id INT);
+    INSERT INTO updated_card (id) SELECT card_artist.card_id FROM card_artist WHERE artist_id = OLD.id;
 END; ~~
 
 CREATE OR REPLACE TRIGGER artist_after_delete
@@ -50,7 +86,29 @@ CREATE OR REPLACE TRIGGER artist_after_delete
     ON artist
     FOR EACH ROW
 BEGIN
-    CALL update_card_artists();
+
+    DECLARE done INT DEFAULT FALSE;
+    DECLARE card_id INT;
+    DECLARE cur CURSOR FOR SELECT id FROM updated_card;
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
+
+    OPEN cur;
+
+    read_loop:
+    LOOP
+        FETCH cur INTO card_id;
+
+        IF done
+        THEN
+            LEAVE read_loop;
+        END IF;
+
+        CALL update_card_artists(card_id);
+    END LOOP;
+
+    CLOSE cur;
+
+    DELETE FROM updated_card;
 END; ~~
 
 
