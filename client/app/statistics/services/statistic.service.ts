1b61c0f1c8cd42398098f0dbde32dbb1ac08744a
Adrien Crivelli <adrien.crivelli@gmail.com>
2022-12-02 17:34:49 +0100

    Debounce statistics recording #9221
    Statistics are slightly delayed, with a debounce, to prevent counting a
    user that would very quickly click on many collections. This kind of
    behavior should not be counted as a real page views. And in a more
    general way, it slows the whole user experience by delaying more
    important queries to be sent. Now the most useful queries have the
    priority.

--- client/app/statistics/services/statistic.service.ts
+++ client/app/statistics/services/statistic.service.ts
@@ -1,60 +1,76 @@
 import {Apollo} from 'apollo-angular';
-import {Inject, Injectable} from '@angular/core';
+import {Inject, Injectable, OnDestroy} from '@angular/core';
 import {SITE} from '../../app.config';
-import {
-    RecordDetail,
-    RecordPage,
-    RecordSearch,
-    Site,
-    Statistic,
-    Statistics,
-    StatisticsVariables,
-    StatisticVariables,
-} from '../../shared/generated-types';
+import {Site, Statistic, Statistics, StatisticsVariables, StatisticVariables} from '../../shared/generated-types';
 import {AbstractContextualizedService} from '../../shared/services/AbstractContextualizedService';
 import {recordDetail, recordPage, recordSearch, statisticQuery, statisticsQuery} from './statistic.queries';
 import {NaturalDebounceService} from '@ecodev/natural';
+import {Observable, Subject, switchMap} from 'rxjs';
+import {debounceTime, takeUntil} from 'rxjs/operators';
+import {DocumentNode} from 'graphql';
 
 @Injectable({
     providedIn: 'root',
 })
-export class StatisticService extends AbstractContextualizedService<
-    Statistic['statistic'],
-    StatisticVariables,
-    Statistics['statistics'],
-    StatisticsVariables,
-    never,
-    never,
-    never,
-    never,
-    never,
-    never
-> {
+export class StatisticService
+    extends AbstractContextualizedService<
+        Statistic['statistic'],
+        StatisticVariables,
+        Statistics['statistics'],
+        StatisticsVariables,
+        never,
+        never,
+        never,
+        never,
+        never,
+        never
+    >
+    implements OnDestroy
+{
+    private readonly page = new Subject<void>();
+    private readonly detail = new Subject<void>();
+    private readonly search = new Subject<void>();
+    private readonly onDestroy = new Subject<void>();
+
     public constructor(apollo: Apollo, naturalDebounceService: NaturalDebounceService, @Inject(SITE) site: Site) {
         super(apollo, naturalDebounceService, 'statistic', statisticQuery, statisticsQuery, null, null, null, site);
+
+        this.createSub(this.page, recordPage);
+        this.createSub(this.detail, recordDetail);
+        this.createSub(this.search, recordSearch);
+    }
+
+    /**
+     * Create a subscription that will send the mutation with a debounced time
+     */
+    private createSub(subject: Observable<void>, mutation: DocumentNode): void {
+        subject
+            .pipe(
+                takeUntil(this.onDestroy),
+                debounceTime(800),
+                switchMap(() =>
+                    this.apollo.mutate<unknown, never>({
+                        mutation: mutation,
+                    }),
+                ),
+            )
+            .subscribe();
     }
 
     public recordPage(): void {
-        this.apollo
-            .mutate<RecordPage>({
-                mutation: recordPage,
-            })
-            .subscribe();
+        this.page.next();
     }
 
     public recordDetail(): void {
-        this.apollo
-            .mutate<RecordDetail>({
-                mutation: recordDetail,
-            })
-            .subscribe();
+        this.detail.next();
     }
 
     public recordSearch(): void {
-        this.apollo
-            .mutate<RecordSearch>({
-                mutation: recordSearch,
-            })
-            .subscribe();
+        this.search.next();
+    }
+
+    public ngOnDestroy(): void {
+        this.onDestroy.next();
+        this.onDestroy.complete();
     }
 }
