73763c1b66f7b376106bb362b3bc0a11cf07f539
David Miserez <david.miserez@unil.ch>
2023-06-06 12:56:03 +0000

    2.5 Add download button in PhotoSwipe view

--- client/app/view-grid/view-grid.component.ts
+++ client/app/view-grid/view-grid.component.ts
@@ -85,6 +85,35 @@ export class ViewGridComponent extends NaturalAbstractController implements OnIn
         activable: true,
         selectable: true,
         lightbox: true,
+        photoSwipePluginsInitFn: lightbox => {
+            lightbox.on('uiRegister', function () {
+                // Download button
+                // https://photoswipe.com/adding-ui-elements/#adding-download-button
+                lightbox.pswp.ui.registerElement({
+                    name: 'download-button',
+                    order: 8,
+                    isButton: true,
+                    tagName: 'a',
+
+                    // SVG with outline
+                    html: {
+                        isCustomSVG: true,
+                        inner: '<path d="M20.5 14.3 17.1 18V10h-2.2v7.9l-3.4-3.6L10 16l6 6.1 6-6.1ZM23 23H9v2h14Z" id="pswp__icn-download"/>',
+                        outlineID: 'pswp__icn-download',
+                    },
+
+                    onInit: (el: any, pswp: any) => {
+                        el.setAttribute('download', '');
+                        el.setAttribute('target', '_blank');
+                        el.setAttribute('rel', 'noopener');
+
+                        pswp.on('change', () => {
+                            el.href = pswp.currSlide.data.src;
+                        });
+                    },
+                });
+            });
+        },
         infiniteScrollOffset: -200,
         ratioLimit: {
             min: 0.5,
