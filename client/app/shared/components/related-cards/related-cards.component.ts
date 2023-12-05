9b14c9242afc1af5328e9a788fc7c63fc684346b
David M <davidmiserez@gmail.com>
2023-12-05 11:09:19 +0000

    Add buttons for moving slidshow left / right

--- client/app/shared/components/related-cards/related-cards.component.ts
+++ client/app/shared/components/related-cards/related-cards.component.ts
@@ -1,21 +1,25 @@
-import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
+import {Component, ElementRef, Input, OnChanges, OnInit, SimpleChanges, ViewChild} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {CardService} from 'client/app/card/services/card.service';
 import {RouterLink} from '@angular/router';
 import {Card, Cards, CardsVariables} from '../../generated-types';
 import {NaturalQueryVariablesManager} from '@ecodev/natural';
 import {Observable, map} from 'rxjs';
+import {MatTooltipModule} from '@angular/material/tooltip';
 
 @Component({
     selector: 'app-related-cards',
     templateUrl: './related-cards.component.html',
     styleUrls: ['./related-cards.component.scss'],
     standalone: true,
-    imports: [CommonModule, RouterLink],
+    imports: [CommonModule, RouterLink, MatTooltipModule],
 })
 export class RelatedCardsComponent implements OnInit, OnChanges {
     @Input({required: true})
     public card!: Card['card'];
+
+    @ViewChild('slideshow')
+    public slideshow!: ElementRef;
 
     public readonly CardService = CardService;
 
@@ -35,6 +39,14 @@ export class RelatedCardsComponent implements OnInit, OnChanges {
         this.updateCardsQueryVariables();
 
         this.cards$ = this.cardService.watchAll(this.cardsQueryVariables).pipe(map(result => result.items));
+    }
+
+    public scrollRight(): void {
+        this.slideshow.nativeElement.scrollLeft += 100;
+    }
+
+    public scrollLeft(): void {
+        this.slideshow.nativeElement.scrollLeft -= 100;
     }
 
     private updateCardsQueryVariables(): void {
