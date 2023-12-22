cd1133a106c5f93be0f391cd0ab78007a1fd5d85
David M <davidmiserez@gmail.com>
2023-12-22 14:10:08 +0000

    Display parents of collection when selecting a collection

--- client/app/shared/hierarchic-configurations/CollectionConfiguration.ts
+++ client/app/shared/hierarchic-configurations/CollectionConfiguration.ts
@@ -1,5 +1,6 @@
 import {NaturalHierarchicConfiguration} from '@ecodev/natural';
 import {CollectionService} from '../../collections/services/collection.service';
+import {formatItemNameWithRoot} from '../services/utility';
 
 export const collectionsHierarchicConfig: NaturalHierarchicConfiguration[] = [
     {
@@ -7,5 +8,6 @@ export const collectionsHierarchicConfig: NaturalHierarchicConfiguration[] = [
         parentsRelationNames: ['parent'],
         childrenRelationNames: ['parent'],
         selectableAtKey: 'collection',
+        displayWith: formatItemNameWithRoot,
     },
 ];
