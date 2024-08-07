e586ddf7d0192c19d86834b0fe514c97a6a0907a
David M <davidmiserez@gmail.com>
2024-08-07 08:27:33 +0000

    Display schema validation diff when involved test fails

--- tests/ApplicationTest/ORM/MappingTest.php
+++ tests/ApplicationTest/ORM/MappingTest.php
@@ -4,6 +4,18 @@ declare(strict_types=1);
 
 namespace ApplicationTest\ORM;
 
+use Doctrine\ORM\Tools\SchemaValidator;
+
 class MappingTest extends \Ecodev\Felix\Testing\ORM\MappingTest
 {
+    public function testMappingIsSync(): void
+    {
+        $em = _em();
+        $validator = new SchemaValidator($em);
+
+        self::assertTrue(
+            $validator->schemaInSyncWithMetadata(),
+            'diff is: ' . json_encode($validator->getUpdateSchemaList()),
+        );
+    }
 }
