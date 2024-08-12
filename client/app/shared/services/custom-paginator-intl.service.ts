0da51d499be56c4d25363a71d9be0eda344a65a4
David M <davidmiserez@gmail.com>
2024-08-12 09:30:01 +0000

    Use same numbering for pagination than before translation

--- client/app/shared/services/custom-paginator-intl.service.ts
+++ client/app/shared/services/custom-paginator-intl.service.ts
@@ -17,9 +17,10 @@ export class CustomPaginatorIntl implements MatPaginatorIntl {
 
     public getRangeLabel(page: number, pageSize: number, length: number): string {
         if (length === 0) {
-            return 'Page 1 de 1';
+            return '0';
         }
-        const amountPages = Math.ceil(length / pageSize);
-        return `Page ${page + 1} de ${amountPages}`;
+        const from = Math.max(page * pageSize, 1);
+        const to = Math.min((page + 1) * pageSize, length);
+        return `${from} - ${to} de ${length}`;
     }
 }
