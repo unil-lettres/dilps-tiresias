d55ad8ea8ae4f723da7a761820439de700ae9d11
Luca Guindani <luca.guindani@gmail.com>
2025-04-25 17:14:51 +0200

    Add mysql container healthcheck & update post-create script

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+echo "Waiting for composer dependencies to be installed..."
+# This is needed to load the test data & enable development mode
+while [ ! -f ./vendor/autoload.php ]; do
+    sleep 1
+done
+
 php ./bin/load-test-data.php --no-interaction
 composer development-enable
