a502cfc7f3c8d76da11a2a839ac3250352410184
Luca Guindani <luca.guindani@gmail.com>
2025-04-29 10:53:13 +0200

    comment post-create commands

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -2,9 +2,9 @@
 
 echo "Waiting for composer dependencies to be installed..."
 # This is needed to load the test data & enable development mode
-while [ ! -f ./vendor/autoload.php ]; do
-    sleep 1
-done
-
-php ./bin/load-test-data.php --no-interaction
-composer development-enable
+#while [ ! -f ./vendor/autoload.php ]; do
+#    sleep 1
+#done
+#
+#php ./bin/load-test-data.php --no-interaction
+#composer development-enable
