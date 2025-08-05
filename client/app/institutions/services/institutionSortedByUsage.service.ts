a702e40cef21c9f07d3256e4271306152e420e89
David M <davidmiserez@gmail.com>
2025-08-05 07:42:46 +0000

    [HOTFIX] Fix institution filtering bug that caused cross-site data pollution
    The InstitutionSortedByUsageService was overriding
    getPartialVariablesForAll() without merging parent variables from
    AbstractContextualizedService, which caused it to ignore the site
    filter. This resulted in institutions being fetched across all sites
    instead of being properly filtered by the current site.
    
    The bug had a cascading effect: when a site used an institution from
    another site, the system would automatically create a duplicate
    institution record for the current site, leading to data pollution.
    
    This fix prevents future data pollution, but existing duplicate
    institution records created by this bug will need to be cleaned up
    manually. Dilps and Tiresias often has the same institutions, and users
    could have chose the wrong one if proposed two institutions spelled
    slightly differently.
    
    The following query will list these institutions:
    
    ```sql
    SELECT
    	institution_1.`name`,
    	institution_1.id,
    	institution_2.id,
    	institution_2.creation_date,
    	institution_2.usage_count,
    	institution_2.site
    FROM
    	institution AS institution_1
    	INNER JOIN institution AS institution_2 ON institution_1.`name` = institution_2.`name`
    WHERE
    	institution_1.site != institution_2.site
    	AND institution_1.id < institution_2.id
    	and institution_2.creation_date > '2025-06-01';
    ```

--- client/app/institutions/services/institutionSortedByUsage.service.ts
+++ client/app/institutions/services/institutionSortedByUsage.service.ts
@@ -1,20 +1,23 @@
 import {Injectable} from '@angular/core';
 import {InstitutionSortingField, InstitutionsVariables, SortingOrder} from '../../shared/generated-types';
 import {InstitutionService} from './institution.service';
-import {Observable, of} from 'rxjs';
+import {Observable, map} from 'rxjs';
 
 @Injectable({
     providedIn: 'root',
 })
 export class InstitutionSortedByUsageService extends InstitutionService {
     public override getPartialVariablesForAll(): Observable<Partial<InstitutionsVariables>> {
-        return of({
-            sorting: [
-                {
-                    field: InstitutionSortingField.usageCount,
-                    order: SortingOrder.DESC,
-                },
-            ],
-        });
+        return super.getPartialVariablesForAll().pipe(
+            map(parentVariables => ({
+                ...parentVariables,
+                sorting: [
+                    {
+                        field: InstitutionSortingField.usageCount,
+                        order: SortingOrder.DESC,
+                    },
+                ],
+            })),
+        );
     }
 }
