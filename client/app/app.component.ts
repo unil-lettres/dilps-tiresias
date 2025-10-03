1e4c939f848fb68aa5cc7bba5ea37fbd64b55d8e
Adrien Crivelli <adrien.crivelli@gmail.com>
2025-10-03 11:51:34 +0900

    SVG favicon

--- client/app/app.component.ts
+++ client/app/app.component.ts
@@ -52,7 +52,7 @@ export class AppComponent implements OnInit {
         const site = this.site;
 
         themeService.init(site, environment.environment);
-        this.favIcon.href = site === Site.Dilps ? 'favicon-dilps.ico' : 'favicon-tiresias.ico';
+        this.favIcon.href = site === Site.Dilps ? 'favicon-dilps.svg' : 'favicon-tiresias.svg';
 
         // Register custom icons.
         [
