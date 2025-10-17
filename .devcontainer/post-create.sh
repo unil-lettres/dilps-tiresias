249152cb91b32ff29e7fc88e08d972eeaff65104
David M. <davidmiserez@gmail.com>
2025-10-17 11:38:17 +0200

    Use native PHP8.4 lazy objects

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -8,6 +8,3 @@ done
 echo "Load dummy data & enable development mode..."
 php ./bin/load-test-data.php --no-interaction
 composer development-enable
-
-echo "Set permissions for ORM cache directory..."
-chmod 777 -R ./data/cache/DoctrineORMModule
