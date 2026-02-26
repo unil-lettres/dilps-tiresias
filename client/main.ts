b581ed7f8df119beb045b97ec3ace5b5fd88101b
Adrien Crivelli <adrien.crivelli@gmail.com>
2026-02-26 19:51:12 +0900

    Angular 21.2 #12255

--- client/main.ts
+++ client/main.ts
@@ -1,7 +1,11 @@
+import {provideZoneChangeDetection} from '@angular/core';
 import {bootstrapApplication} from '@angular/platform-browser';
 import {appConfig} from './app/app.config';
 import {AppComponent} from './app/app.component';
 
-bootstrapApplication(AppComponent, appConfig).catch((err: unknown) => {
+bootstrapApplication(AppComponent, {
+    ...appConfig,
+    providers: [provideZoneChangeDetection(), ...appConfig.providers],
+}).catch((err: unknown) => {
     console.error(err);
 });
