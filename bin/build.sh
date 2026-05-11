e40aac9f95979723fd9aad1f56edb55509bcc22e
David M. <davidmiserez@gmail.com>
2026-05-11 15:30:32 +0200

    Add frozen-lockfile for pnpm install in build.sh

--- bin/build.sh
+++ bin/build.sh
@@ -44,7 +44,7 @@ echo "********************* Installing git hooks..."
 ln -fs ../../bin/pre-commit.sh .git/hooks/pre-commit
 
 echo "********************* Updating Node.js packages..."
-pnpm install
+pnpm install --frozen-lockfile
 
 echo "********************* Updating all PHP dependencies via composer..."
 composer install --classmap-authoritative --no-interaction --no-plugins $NO_PROGRESS
